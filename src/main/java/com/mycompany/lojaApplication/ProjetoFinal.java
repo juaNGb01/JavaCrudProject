ca0acccabf15a6c6c21374fb7d94dc9deb9156db
juaNGb01 <juanbogobenitez@gmail.com>
2025-01-30 20:21:32 -0300

    Add files via upload
    feat:Adicionar tela de cadastro de produtos e tela de veda
                 Criação de tela de venda de produtos e cadastro de novos produtos"
                 
              fix: Reoarganizar os menus de navegação
                 implementado organização de menus para melhora na navegação
                  na aplicação

---
+++ src/main/java/com/mycompany/lojaApplication/ProjetoFinal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lojaApplication;
+
+import DBConnection.Conexao;
+import java.sql.Connection;
+
+/**
+ *
+ * @author juanb
+ */
+public class ProjetoFinal {
+
+    public static void main(String[] args) {
+        
+       char[] senha = {'r', 'o', 'o', 't'}; 
+       
+       //Connection conn = Conexao.getConnection("postgres", senha); 
+        
+    }
+}
