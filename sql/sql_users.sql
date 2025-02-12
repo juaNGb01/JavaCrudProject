db6a144b1aa29436467bb5c4308300d116c4b2c6
Julio Sontag <juliosontag@gmail.com>
2025-02-12 17:34:29 -0300

    Update sql_users.sql

--- sql/sql_users.sql
+++ sql/sql_users.sql
@@ -16,13 +16,6 @@ CREATE USER fornecedor2 WITH PASSWORD 'funcionario2';
 GRANT role_funcionario TO fornecedor2
 
 
-insert into tb_usuarios(1, 'administrador', 'administrador', 'admin'); 
-insert into tb_usuarios(2, 'gerente', 'gerente', 'gerente'); 
-insert into tb_usuarios(3, 'funcionario', 'funcionario', 'funcionario'); 
-insert into tb_usuarios(4, 'fornecedor1', 'fornecedor1', 'fornecedor'); 
-insert into tb_usuarios(5, 'fornecedor2', 'fornecedor2', 'fornecedor'); 
-
-
 insert into tb_funcionarios(1, 'administrador','12345678902' 'administrador', 'admin'); 
 insert into tb_funcionarios(2, 'gerente','78945612393' 'gerente', 'gerente'); 
 insert into tb_funcionarios(3, 'funcionario','78945612385' ,'funcionario', 'funcionario'); 
