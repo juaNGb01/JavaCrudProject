3fb6f36a4b58fad1d668bf47024f8600cd074a60
Julio Sontag <juliosontag@gmail.com>
2025-02-12 17:36:55 -0300

    Update sql_users.sql

--- sql/sql_users.sql
+++ sql/sql_users.sql
@@ -5,17 +5,18 @@ CREATE USER gerente WITH PASSWORD 'gerente';
 GRANT role_gerente TO gerente;
 
 CREATE USER funcionario WITH PASSWORD 'funcionario'; 
-GRANT role_funcionario TO funcionario
+GRANT role_funcionario TO funcionario;
 
 
 CREATE USER fornecedor1 WITH PASSWORD 'funcionario1'; 
-GRANT role_funcionario TO fornecedor1
+GRANT role_funcionario TO fornecedor1;
 
 
 CREATE USER fornecedor2 WITH PASSWORD 'funcionario2'; 
-GRANT role_funcionario TO fornecedor2
+GRANT role_funcionario TO fornecedor2;
+SELECT * FROM tb_usuarios;
 
 
-insert into tb_funcionarios(1, 'administrador','12345678902' 'administrador', 'admin'); 
-insert into tb_funcionarios(2, 'gerente','78945612393' 'gerente', 'gerente'); 
-insert into tb_funcionarios(3, 'funcionario','78945612385' ,'funcionario', 'funcionario'); 
+insert into tb_funcionarios VALUES(1,'administrador','12345678902' ,'administrador', 'admin'); 
+insert into tb_funcionarios VALUES (2, 'gerente','78945612393' ,'gerente', 'gerente'); 
+insert into tb_funcionarios VALUES (3, 'funcionario','78945612385' ,'funcionario', 'funcionario'); 
