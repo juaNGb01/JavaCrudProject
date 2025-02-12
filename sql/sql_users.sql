71a3f1083063b2b8fca9fadae83044659bff4f75
juaNGb01 <juanbogobenitez@gmail.com>
2025-02-12 19:30:36 -0300

    Update sql_users.sql

--- sql/sql_users.sql
+++ sql/sql_users.sql
@@ -1,6 +1,16 @@
-CREATE USER administrador WITH PASSWORD 'administrador'; 
-GRANT role_admin to administrador; 
 
+
+
+--Add user por meio da procedure - nome do usuário/cpf/senha/funcao
+--adm
+CALL adduserandgrantrole('administrador', '12345678910', 'admin123', 'admin')
+--gerente
+CALL adduserandgrantrole('gerente', '74185296310', 'geret123', 'gerente');
+--funcionario
+CALL adduserandgrantrole('funcionario', '78945632152', 'fun123', 'funcionario');
+CALL adduserandgrantrole('funcionario2', '32198745630', 'fun321', 'funcionario');
+
+--convencional
 CREATE USER gerente WITH PASSWORD 'gerente';
 GRANT role_gerente TO gerente;
 
@@ -8,14 +18,8 @@ CREATE USER funcionario WITH PASSWORD 'funcionario';
 GRANT role_funcionario TO funcionario;
 
 
-CREATE USER fornecedor1 WITH PASSWORD 'funcionario1'; 
-GRANT role_funcionario TO fornecedor1;
-
-
-CREATE USER fornecedor2 WITH PASSWORD 'funcionario2'; 
-GRANT role_funcionario TO fornecedor2;
-SELECT * FROM tb_usuarios;
-
+CREATE USER administrador WITH PASSWORD 'administrador'; 
+GRANT role_admin to administrador; 
 
 insert into tb_funcionarios VALUES(1,'administrador','12345678902' ,'administrador', 'admin'); 
 insert into tb_funcionarios VALUES (2, 'gerente','78945612393' ,'gerente', 'gerente'); 
